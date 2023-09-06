2c9624405773a874f0f4ca19b5a57aceeb2d29c0
non <tadchawan@geniustree.co.th>
2023-09-06 17:41:54 +0700

    add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,47 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {TextComponent} from "./text/text.component";
+import {DisplayComponent} from "./display/display.component";
+import {TableComponent} from "./table/table.component";
+import {AlertsComponent} from "./alerts/alerts.component";
+import {ButtonComponent} from "./button/button.component";
+import {NavComponent} from "./nav/nav.component";
+import {MenuComponent} from "./menu/menu.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "display",
+    component: DisplayComponent
+  },
+  {
+    path: "text",
+    component: TextComponent
+  },
+  {
+    path:"table",
+    component:TableComponent
+  },
+  {
+    path:"alert",
+    component:AlertsComponent
+  },
+  {
+    path:"button",
+    component:ButtonComponent
+  },
+  {
+   path:"nav",
+   component:NavComponent
+  },
+  {
+    path:"menu",
+    component:MenuComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TextComponent } from './text/text.component';
+import { DisplayComponent } from './display/display.component';
+import { TableComponent } from './table/table.component';
+import { AlertsComponent } from './alerts/alerts.component';
+import { ButtonComponent } from './button/button.component';
+import { NavComponent } from './nav/nav.component';
+import {AutoSizeInputModule} from "ngx-autosize-input";
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextComponent,
+    DisplayComponent,
+    TableComponent,
+    AlertsComponent,
+    ButtonComponent,
+    NavComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    AutoSizeInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
